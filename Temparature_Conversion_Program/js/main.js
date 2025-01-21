0f74fb4428eab6b8b6e77abeb557658cb43bccb4
Chu Long <chulong_t65@hus.edu.vn>
2025-01-22 01:31:35 +0700

    JavaScript _ Temperature Conversion Program

---
+++ Temparature_Conversion_Program/js/main.js
@@ -0,0 +1,33 @@
+const input_number = document.querySelector("#input_number");
+
+const fahrenheit = document.querySelector("#toFahrenheit");
+const celsius = document.querySelector("#toCelsius");
+
+const submit = document.querySelector(".submit");
+
+const result = document.querySelector("#result-message");
+
+function convert(number) {
+    if (number === "") {
+        result.textContent = "Input your number";
+        return;
+    }
+
+    if (fahrenheit.checked) {
+        result.textContent = (number * (9 / 5) + 32).toFixed(1) + "°F";
+    } else if (celsius.checked) {
+        result.textContent = ((number - 32) * (5 / 9)).toFixed(1) + "°C";
+    } else {
+        result.textContent = "Select a unit";
+    }
+}
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    convert(input_number.value);
+});
+
+window.addEventListener("load", () => {
+    input_number.focus();
+});
