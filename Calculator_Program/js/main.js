21dc948515b3ceb7264dcdc77bed2cd22774f599
Chu Long <chulong_t65@hus.edu.vn>
2025-01-24 23:40:14 +0700

    JavaScript - Calculator Program

---
+++ Calculator_Program/js/main.js
@@ -0,0 +1,17 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(s) {
+    display.value += s;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
+}
